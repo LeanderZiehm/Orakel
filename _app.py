f7ba8a24ffe23f7517106274d0330e4cb53d692a
AiSmartVision <leander-arun.ziehm@stud.th-deg.de>
2024-11-13 00:42:38 +0100

    added graph

--- _app.py
+++ _app.py
@@ -17,22 +17,17 @@ Your answer should be a Python list in this format: ["item1", "item2", "item3"]
 Do not add any extra words or explanations outside the array.
 Only list items that directly answer the question\nUser:{user_message}"""
 
-    print(f"User:{user_message}")
+    print(user_message)
     response = ollama.chat(model='llama3.1', messages=[
     {
         'role': 'user',
         'content': prompt
     }
     ])
-    # print(response['message']['content'])
+    print(response['message']['content'])
+
     chatbot_message = response['message']['content']#f"I got:{user_message}. I give: " + response['message']['content']
-    # remove [ and ] and split by comma
-    # remove char [
-    print(f"Chatbot Message:{chatbot_message}")
-    chatbot_message = chatbot_message.replace('"','').replace('[','').replace(']','')
-    items = chatbot_message.split(',')
-    print(f"Items:{items}")
-    return jsonify({'response':items})
+    return jsonify({'response': chatbot_message})
 
 if __name__ == '__main__':
     # app.run(debug=True,host='0.0.0.0')#
