38c340d3388da8eb9135ef73109e03ebabec5613
AiSmartVision <leander-arun.ziehm@stud.th-deg.de>
2024-11-16 01:36:01 +0100

    added image generation

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask, render_template, request, jsonify
-import ollama
+from io import BytesIO
+import base64
+from PIL import Image
+import requests
 
 # populate exec env
 env = ""
@@ -14,6 +17,17 @@ env = {k: v for k, v in (tuple(t.split("=")) for t in [l for l in env.split()])}
 # modelName = 'tinyllama'
 # modelName = 'llama3.1'
 modelName = env["MODEL"] if "MODEL" in env else "llama3.1"
+
+if modelName != "None":
+    import ollama
+
+if env["StableDiffusion"] != "None":
+    from diffusers import AutoPipelineForText2Image
+    import torch
+
+    pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sd-turbo", torch_dtype=torch.float16, variant="fp16")
+    pipe.to("cuda")
+
 
 app = Flask(__name__)
 
@@ -52,7 +66,61 @@ Only list items that directly answer the question"""
     return jsonify({"response": items})
 
 
+def generate_image(prompt):
+    # Generate the image based on the prompt
+    image = pipe(prompt=prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
+
+    # Save the image to a byte buffer
+    buffered = BytesIO()
+    image.save(buffered, format="PNG")
+
+    # Encode the image to base64
+    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+    return img_str
+
+
+# Define the route for generating images
+@app.route("/image", methods=["POST"])
+def generate():
+
+    if env["StableDiffusion"] == "None":
+        image_url = "https://picsum.photos/1024/1024"
+
+        # Fetch the image from the URL
+        response = requests.get(image_url)
+
+        if response.status_code == 200:
+            # Open the image from the response content
+            img = Image.open(BytesIO(response.content))
+
+            # Convert image to a byte stream
+            img_byte_arr = BytesIO()
+            img.save(img_byte_arr, format="PNG")  # or 'JPEG', based on your needs
+            img_byte_arr = img_byte_arr.getvalue()
+
+            # Encode image bytes to base64
+            img_base64 = base64.b64encode(img_byte_arr).decode("utf-8")
+
+            # Return base64 string in a JSON response
+            return jsonify({"image_base64": img_base64})
+
+    else:
+        # Get the prompt from the JSON request
+        data = request.json
+        prompt = data.get("prompt", "")
+
+        # Generate the image if prompt is not empty
+        if prompt:
+            generated_image = generate_image(prompt)
+            return jsonify({"image_base64": generated_image})
+        else:
+            return jsonify({"error": "No prompt provided"}), 400
+
+
 if __name__ == "__main__":
     PORT = int(env["PORT"]) if "PORT" in env else 5002
     HOST = env["HOST"] if "HOST" in env else "127.0.0.1"
     app.run(debug=True, port=PORT, host=HOST)
+
+
+##########
